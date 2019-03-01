f6c1949adf9b623b12672e2147297dc7a43353fc
Igor Osadchyi <igor@webio.net.ua>
2019-03-01 19:03:58 +0200

    Email Support

--- app/models.py
+++ app/models.py
@@ -1,8 +1,11 @@
 from hashlib import md5
 from datetime import datetime
+from time import time
+
+import jwt
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-from app import db, login
+from app import db, login, app
 
 
 @login.user_loader
@@ -43,6 +46,20 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
 
     def follow(self, user):
         if not self.is_following(user):
